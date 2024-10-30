d12540d32470c1ca1602dabfdc4e3da71ed3801f
Lei Wang <rakgatech@gmail.com>
2024-10-29 23:14:52 -0700

    Create testing code structure. Add a dummy test method.

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/LinearSlideTestingTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class LinearSlideTestingTest {
+
+    @Test
+    public void test_outtakeMode() {
+        assertEquals(1,0+1);
+    }
+}
